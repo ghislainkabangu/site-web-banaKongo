26dd8ef333353ed61c97c54eba36ccbb1b881002
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-06-04 16:48:21 +0100

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,21 @@
+
+let nav = document.getElementsByTagName("nav")[0];
+let openMenu = document.querySelector(".open_menu");
+let closeMenu = document.querySelector(".close_menu");
+
+openMenu.addEventListener("click", () => {
+  nav.classList.add("navbarr_Js");
+  openMenu.classList.add("close_menu_js_effet");
+});
+closeMenu.addEventListener("click", () => {
+  nav.classList.remove("navbarr_Js");
+  openMenu.classList.remove("close_menu_js_effet");
+});
+/******************************links (tous les liens)**************** */
+let links = document.querySelectorAll(".link");
+for (let z = 0; z < links.length; z++) {
+  links[z].addEventListener("click", () => {
+    nav.classList.remove("navbarr_Js");
+    openMenu.classList.remove("close_menu_js_effet");
+  });
+}
